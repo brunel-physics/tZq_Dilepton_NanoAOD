4a6e54bf8a39ef766fa1335e1b17dd58db9b1750
KathrynColdham <kathryn.coldham@cern.ch>
2020-06-15 07:22:58 +0100

    Still trying to add branches to the tree.

--- src/fulleventselectionAlgo.cpp
+++ src/fulleventselectionAlgo.cpp
@@ -12790,13 +12790,16 @@ if(blinding == true && (SBR == true || SR == true)){
 
 	//Writing the histograms for the ee channel to an output root file	
 	TFile * output_ee = new TFile(OutRootFile_ee.c_str(), "RECREATE");
-	output_ee->cd();
+        output_ee->cd();
 
-	TTree * tree = new TTree("tree", "tree");
-	tree->Write();
+        ROOT::RDF::RResultPtr<TH1D> histo_ee[N_Columns_ee] = {};
 
-	ROOT::RDF::RResultPtr<TH1D> histo_ee[N_Columns_ee] = {};
 
+	std::string branch = "Nominal";
+	ROOT::RDF::RSnapshotOptions opts;
+	opts.fMode = "UPDATE";
+
+	auto snapshot_ee = AfterChi2Cut_ee.Snapshot(branch.c_str(), OutRootFile_ee.c_str(), ".*");
 
         for(int i = 0; i < N_Columns_ee; i++){
 
@@ -12817,8 +12820,7 @@ if(blinding == true && (SBR == true || SR == true)){
                         std::cout << "ColName = " << ColName << std::endl;
 
                         histo_ee[i] = AfterChi2Cut_ee.Histo1D(ColName.c_str(), "EventWeight");
-                        histo_ee[i]->Write();
-			tree->Fill();
+			histo_ee[i]->Write();			
 
                 }
                 else if(ColName  == "PU"                      || ColName == "BTagWeight"                || ColName == "ReturnedPSWeight"          ||
@@ -12827,8 +12829,6 @@ if(blinding == true && (SBR == true || SR == true)){
                         ColName  == "ME_SF"                   || ColName == "ReweightedTopPt"           || ColName == "EventWeight" 		  ||
 			ColName  == "chi2" 		      || ColName == "AfterChi2Cut_ee"){
 
-                        histo_ee[i] = AfterChi2Cut_ee.Histo1D(ColName.c_str());
-                        histo_ee[i]->Write();
 
                 }
                 else{std::cout << "Check ColName for ee channel - ColName is = " << ColName << std::endl; continue;}
@@ -12836,8 +12836,8 @@ if(blinding == true && (SBR == true || SR == true)){
 
         }
 
-	output_ee->Close();
 
+	auto snapshot_ee_updated = AfterChi2Cut_ee.Snapshot(branch.c_str(), OutRootFile_ee.c_str(), ".*", opts);
 
 
 	//Writing the histograms for the mumu channel to an output root file      
