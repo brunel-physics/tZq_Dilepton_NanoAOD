e8f5b5b4b20d7c57e4e2a1d3cc84b0bd219f15b7
KathrynColdham <kathryn.coldham@cern.ch>
2021-03-28 09:46:00 +0100

    Test code for makefiles

---
+++ TestCode/main.cpp
@@ -0,0 +1,13 @@
+#include "tZq_Dilepton_Analysis.h"
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+  AnalysisClass tZq;
+  tZq.RunAnalysisFunction();
+
+  return 0;
+
+}
---
+++ TestCode/tZq_Dilepton_Analysis.h
@@ -0,0 +1,11 @@
+#ifndef TZQ_DILEPTON_ANALYSIS_H
+#define TZQ_DILEPTON_ANALYSIS_H
+
+class AnalysisClass{
+
+  public:
+	void RunAnalysisFunction();
+
+};
+
+#endif // TZQ_DILEPTON_ANALYSIS_H
