0a77191c24a2f10c6b1caefb5363f8e3f993d16c
KathrynColdham <kathryn.coldham@cern.ch>
2021-03-30 09:33:22 +0100

    modified

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-#2016
-
-#nominal
-#mumu
-tmux new-session -d -s tZq_mumu_2016 'make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0'
+tmux new-session -d -s tZq_mumu_2016 './bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0'
