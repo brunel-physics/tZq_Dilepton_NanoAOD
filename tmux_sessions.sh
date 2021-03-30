386887e61a715ae890755e72bbb2ad9aab4581dd
KathrynColdham <kathryn.coldham@cern.ch>
2021-03-30 22:35:08 +0100

    Trying more commands

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #tmux new-session -d -s tZq_mumu_2016 'make; make clean; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0'
 #tmux new-session -d -s test_tmux 'vim test.txt'
-tmux new-session -d -s tZq_mumu_2016 'vim Commands.sh; printf "#!/bin/bash \n make \n make clean \n ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0" >> Commands.sh; chmod+x Commands.sh; ./Commands.sh'
+vim Commands.sh
+chmod +x Commands.sh
+tmux new-session -d -s tZq_mumu_2016 'printf "#!/bin/bash\nmake\nmake clean\n./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0" >> Commands.sh'
