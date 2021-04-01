402cb32ee398c1f8ff8494328097741bf3e6a7c3
KathrynColdham <kathryn.coldham@cern.ch>
2021-04-01 11:16:22 +0100

    Shell script working

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-#tmux new-session -d -s tZq_mumu_2016 'make; make clean; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0'
-#tmux new-session -d -s test_tmux 'vim test.txt'
-vim Commands.sh
-chmod +x Commands.sh
-tmux new-session -d -s tZq_mumu_2016 'printf "#!/bin/bash\nmake\nmake clean\n./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0" >> Commands.sh'
+
+ProcessNamesArray_mumu_2016_NominalRuns=("tZq_mumu_2016_nominal" "tZq_scaleup_mumu_2016_nominal" "tZq_scaledown_mumu_2016_nominal")
+
+for i in ${!ProcessNamesArray_mumu_2016_NominalRuns[@]}; do
+	if [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_mumu_2016_nominal" ]
+		then tmux new -d -s tZq_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
+	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_scaleup_mumu_2016_nominal" ]
+                then tmux new -d -s tZq_scaleup_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 1 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
+	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_scaledown_mumu_2016_nominal" ]
+                then tmux new -d -s tZq_scaledown_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 2 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
+	else 
+		echo "ERROR: Check ProcessNamesArray"
+		break 
+	fi 
+done
+
+echo "The tmux sessions for the SBR (mumu channel 2016) are running"
+
+
+
+
