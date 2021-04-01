8ca0f0e415bc13217719b9df722288b73faedd0f
KathrynColdham <kathryn.coldham@cern.ch>
2021-04-01 11:55:49 +0100

    Sorting variable names

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
-    ############################################################# 2016 (MC, mumu chiannel) #################################################################################
-
-ProcessNamesArray_mumu_2016_NominalRuns=("tZq_mumu_2016_nominal"  "tZq_scaleup_mumu_2016_nominal" "tZq_scaledown_mumu_2016_nominal"
-					 "ZPlusJets_M50_aMCatNLO" "ZPlusJets_M50_Madgraph")
+    ############################################################# 2016 (MC, mumu channel) #################################################################################
 
 #Nominal runs
-for i in ${!ProcessNamesArray_mumu_2016_NominalRuns[@]}; do
 
-	if [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_mumu_2016_nominal" ]
+ProcessNamesArray=("tZq_mumu_2016_nominal"          "tZq_scaleup_mumu_2016_nominal"   "tZq_scaledown_mumu_2016_nominal"
+		   "ZPlusJets_M50_aMCatNLO"         "ZPlusJets_M50_aMCatNLO_ext"      "ZPlusJets_M50_Madgraph"
+		   "ZPlusJets_M50_Madgraph_ext"     "ZPlusJets_M10To50_aMCatNLO"      "ZPlusJets_M10To50_aMCatNLO_ext" 
+		   "ZPlusJets_M10To50_Madgraph"     "ZPlusJets_M10To50_Madgraph_ext"  "ZPlusJets_PtBinned_0To50"       
+                   "ZPlusJets_PtBinned_50To100"     "ZPlusJets_PtBinned_50To100_ext"  "ZPlusJets_PtBinned_100To250")
+
+
+for i in ${!ProcessNamesArray[@]}; do
+
+	if [ ${ProcessNamesArray[$i]} = "tZq_mumu_2016_nominal" ]
 
 		then tmux new -d -s tZq_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 0 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
 
-	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_scaleup_mumu_2016_nominal" ]
+	elif [ ${ProcessNamesArray[$i]} = "tZq_scaleup_mumu_2016_nominal" ]
 
                 then tmux new -d -s tZq_scaleup_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 1 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
 
-	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "tZq_scaledown_mumu_2016_nominal" ]
+	elif [ ${ProcessNamesArray[$i]} = "tZq_scaledown_mumu_2016_nominal" ]
 
                 then tmux new -d -s tZq_scaledown_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 2 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
 
-	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "ZPlusJets_M50_aMCatNLO" ]
+	elif [ ${ProcessNamesArray[$i]} = "ZPlusJets_M50_aMCatNLO" ]
 
                 then tmux new -d -s ZPlusJets_M50_aMCatNLO_mumu_2016_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 3 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
 
-	elif [ ${ProcessNamesArray_mumu_2016_NominalRuns[$i]} = "ZPlusJets_M50_Madgraph" ]
+	elif [ ${ProcessNamesArray[$i]} = "ZPlusJets_M50_Madgraph" ]
 
                 then tmux new -d -s ZPlusJets_M50_Madgraph_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 5 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
 
+	elif [ ${ProcessNamesArray[$i]} = "ZPlusJets_M50_Madgraph_ext" ]
+
+                then tmux new -d -s ZPlusJets_M50_Madgraph_ext_nominal 'source ~/.bashrc; make clean; make; ./bin/fulleventselectionMain.exe --mc 1 -y 2016 -p 6 --npl 0 --sr 1 --sbr 1 --zjcr 0 --ttcr 0 --sys 0 --channel 2 --dcc 0; sleep 30'
+
 	else 
-		echo "ERROR: Check ProcessNamesArray"
+		echo "ERROR: Check ProcessNamesArray for " ${ProcessNamesArray[$i]}
 		break 
 
 	fi 
